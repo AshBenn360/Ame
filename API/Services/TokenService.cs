8be711c11dc5ae1071a3cd81639d5e1a61a17b55
AshBenn360 <ashleysbennett@icloud.com>
2023-06-07 20:32:11 +0100

    latest changes

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -21,7 +21,7 @@ namespace API.Services
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.Role), user.Role)
+                new Claim(ClaimTypes.Role, user.Role)
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
